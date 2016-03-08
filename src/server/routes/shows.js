738778b00ab4c029b3299fe48cc32885436cd183
Dennis Bailey <bailey.dennis@gmail.com>
2016-03-08 12:18:29 -0700

    serverside route testing for get all, get one, insert, update, and delete routes

--- src/server/routes/shows.js
+++ src/server/routes/shows.js
@@ -6,8 +6,67 @@ function Shows() {
     return knex('shows');
 };
 
+
 router.get('/shows', function(req, res, next) {
-  res.status(200).json('testing');
+//   res.status(200).json('testing');
+  Shows().select()
+  
+  .then( function (result) {  
+      res.status(200).json(result);
+  });
+  
+});
+
+
+router.get('/shows/:id', function (req, res, next){
+
+  Shows().select().where('id', req.params.id)
+  
+  .then( function (result) {  
+      res.status(200).json(result);
+  });
+  
+});
+
+
+router.post('/shows', function(req, res, next) {
+
+  Shows().insert({
+      name : req.body.name,
+      channel : req.body.channel,
+      genre : req.body.genre,
+      rating : req.body.rating,
+      explicit : req.body.explicit
+  })
+  
+  .then( function (result) {  
+      res.status(200).json(result);
+  });
+  
+});
+
+
+router.put('/shows/:id', function(req, res, next) {
+  
+  Shows().update(req.body).where('id', req.params.id)
+  
+  .then( function (result) {  
+      res.status(200).json(result);
+  });
+  
+});
+
+
+router.delete('/shows/:id', function(req, res, next) {
+
+  Shows().where('id', req.params.id).del()
+  
+  .then( function (result) {
+    
+    console.log('deleted');  
+      res.status(200).json(result);
+  });
+  
 });
 
 module.exports = router;
--- test/apiTest.js
+++ test/apiTest.js
@@ -52,5 +52,122 @@ describe('API routes', function() {
 
     });
 
+    describe('Add a single show', function() {
+      
+        it('should POST a single show', function(done) {
+          
+            chai.request(server)
+            
+            .post('/api/shows')
+            
+            .send({
+                name: 'New Show',
+                channel: 'ABC',
+                genre: 'Garbage',
+                rating: 1,
+                explicit: false
+            })
+            
+            .end(function(err, res) {
+                
+                chai.request(server)
+                
+                .get('/api/shows/5')
+                
+                .end(function(err, res) {
+
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.should.be.a('array');
+                    res.body.length.should.equal(1);
+                 
+                    res.body[0].should.have.property('name');
+                    res.body[0].should.have.property('channel');
+                    res.body[0].should.have.property('genre');
+                    res.body[0].should.have.property('rating');
+                    res.body[0].should.have.property('explicit');
+
+                    res.body[0].name.should.equal('New Show');
+                    res.body[0].channel.should.equal('ABC');
+                    res.body[0].genre.should.equal('Garbage');
+                    res.body[0].rating.should.equal(1);
+                    res.body[0].explicit.should.equal(false);
+                    
+                    done();
+                  
+                });
+              
+            });
+          
+        });
+    
+    });
+    
+    
+    describe('It should edit a single show', function() {
+      
+        it('should edit a show', function(done) {
+          
+            chai.request(server)
+            
+            .put('/api/shows/1')
+            
+            .send({
+                name : 'Edited Name'
+            })
+            
+            .end(function(err, res){
+                chai.request(server)
+                
+                .get('/api/shows/' + res.body)
+                
+                .end(function(err, res) {
+                    console.log('err:', err);
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    
+                    res.body[0].name.should.equal('Edited Name');
+                  
+                    done();
+                  
+                });
+              
+            });
+                
+        });
+      
+    });
+    
+    describe('Delete a single show', function() {
+      
+      it('should delete a single show', function(done) {
+        
+        chai.request(server)
+        
+        .delete('/api/shows/1')
+        
+        .end(function(err, res) {
+
+            chai.request(server)
+            
+            .get('/api/shows/1')
+            
+            .end(function(err, res) {
+
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body.length.should.equal(0);
+                
+                done();
+              
+            })
+              
+        });
+      
+      });
+      
+    });
+    
 });
 
